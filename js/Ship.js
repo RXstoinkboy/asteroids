342d7b2b58620e628450dd14cf795558c5f9bf48
RXstoinkboy <erykludwin@gmail.com>
2018-09-09 02:05:12 +0200

    added full stearing. NEXT: have to draw flame behind a ship when it accelerates

--- js/Ship.js
+++ js/Ship.js
@@ -1,29 +1,81 @@
+// ship constructor - ship is written into a circle
 class Ship {
     constructor() {
+        //  circle frame radius
         this.r = 0.04;
+        // angle of rear part ofthe ship (alternating between + and minus)
         this.rearA = 50;
+        // front of the ship angle
         this.a = 0;
         this.x = VAR.W / 2;
         this.y = VAR.H / 2;
+
+
+        //addition ship props for acceleration
+        // x difference from the middle of canvas
+        this.modX = 0;
+        this.modY = 0;
+        // acceleration
+        this.acc = 0.0004;
+
+        // max speed
+        this.maxMod = 0.019;
+
+        // container for ship's coordinates
         this.points = [{}, {}, {}];
     }
 
+    // draw method
     draw() {
+        // ship's controls included to dynamicly adjust ship angle
         if (Game.key_37 || Game.key_39) {
+            // rotation speed = 7 deg/frame
             this.a = this.a + 7 * (Game.key_37 ? -1 : 1);
         }
 
+        // adding acceleration
+        if (Game.key_38) {
+            this.modX = Math.max(-this.maxMod * VAR.d, Math.min(this.maxMod * VAR.d, this.modX + Math.sin(Math.PI / 180 * this.a) * this.acc * VAR.d));
+            this.modY = this.modY - Math.cos(Math.PI / 180 * this.a) * this.acc * VAR.d;
+
+            // ship slows down after relesaing a button
+        } else {
+            this.modX = this.modX * 0.98;
+            // let it stop after it reaches very low speed
+            this.modX = Math.abs(this.modX) < 0.0001 ? 0 : this.modX;
+
+            this.modY = this.modY * 0.98;
+            this.modY = Math.abs(this.modY) < 0.0001 ? 0 : this.modY;
+
+        }
+
+        // speeding up
+        this.x += this.modX;
+        this.y += this.modY;
+
+
+        // drawing function for every peak of triangle
         Game.ctx.beginPath();
         for (let i = 0; i < 3; i++) {
+            // loop through every peak of triangle
+            // new temporary angle (tempA) used for drawing
             this.tempA = i === 0 ? this.a : (this.a + 180 + (i == 1 ? this.rearA : -this.rearA));
             this.tempR = i === 0 ? this.r : (this.r * 0.7);
+            // saving coordinated in 'this.points array' - point's coordinates calculated based on middle of circle
             this.points[i].x = Math.sin(Math.PI / 180 * this.tempA) * this.tempR * VAR.d + this.x;
             this.points[i].y = -Math.cos(Math.PI / 180 * this.tempA) * this.tempR * VAR.d + this.y;
-
+            // telling canvas to draw lines between points - one stroke is missing
             Game.ctx[i === 0 ? 'moveTo' : 'lineTo'](this.points[i].x, this.points[i].y);
 
         }
+        // adding last stroke with 'closePath' to close a triangle shape
         Game.ctx.closePath();
         Game.ctx.stroke();
+
+        // drawing flames behind ship
+        Game.ctx.beginPath();
+        if (Game.key_38) {
+
+        }
     }
 } 
--- js/game.js
+++ js/game.js
@@ -23,34 +23,50 @@ VAR = {
 // Game to nie jest typowy zmienna, tylko obiekt przypisany do window
 Game = {
     init: () => {
+
+        // drawing canvas
         Game.canvas = document.createElement('canvas');
         Game.ctx = Game.canvas.getContext('2d');
+
+        // initializing layout function
         Game.layout();
 
+        // dynamic resize tracking
         window.addEventListener('resize', Game.layout, false)
 
         document.body.appendChild(Game.canvas);
 
+        // calling ship instance
         Game.ship = new Ship()
 
+        // event listeners for ship controls
         window.addEventListener('keydown', Game.onKey, false);
         window.addEventListener('keyup', Game.onKey, false);
 
+        // animation initialisation
         Game.animationLoop();
     },
 
+    // steering
     onKey: (e) => {
-        if (e.keyCode == 32 || e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39) {
-            // e.preventDefault();
 
+        // working only with arrows and 'space'
+        if (e.keyCode == 32 || e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39) {
+            e.preventDefault();
+
+            // ship rotation
+            // checkes if a give key is not already pressed
             if (e.type == 'keydown' && !Game['key_' + e.keyCode]) {
                 Game['key_' + e.keyCode] = true;
+
+                // if two keys are pressed, new one will work
                 if (e.keyCode == 37) {
                     Game.key_39 = false;
                 } if (e.keyCode == 39) {
                     Game.key_37 = false;
                 }
 
+                // stop rotation after key release
             } else if (e.type == 'keyup') {
                 Game['key_' + e.keyCode] = false;
             }
@@ -58,6 +74,7 @@ Game = {
         };
     },
 
+    // dynamic dimension calculations
     layout: (e) => {
         VAR.W = window.innerWidth;
         VAR.H = window.innerHeight;
@@ -72,12 +89,14 @@ Game = {
         Game.ctx.lineJoin = 'round';
     },
 
+    // main animation function
     animationLoop: (time) => {
         requestAnimationFrame(Game.animationLoop);
         if (time - VAR.lastTime >= 1000 / VAR.fps) {
             VAR.lastTime = time;
+            // clearing previous frame            
             Game.ctx.clearRect(0, 0, VAR.W, VAR.H);
-
+            // drawing ship instance in every frame
             Game.ship.draw();
         };
     },
